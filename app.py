9333ebadf4de7c7510d60bc91bb85fdec06a4423
roshan-prabakaran <sit24ad085@sairamtap.edu.in>
2026-04-25 12:47:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,5 +52,8 @@ def predict():
             "error": str(e)
         })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
+    app.run(host="0.0.0.0", port=port)
